0b9f7fdcef39adf21deb02b727da3794572a5e12
KeonWoo <pkw8056@gmail.com>
2021-06-20 16:28:44 +0900

    Add BJ 7576

--- algorithm/temp.py
+++ algorithm/temp.py
@@ -1,5 +1,14 @@
 import sys
 from collections import deque
 
-if 2 :
-    print('abd')
+queue = deque()
+
+queue.appendleft((1,1))
+print(queue)
+
+
+a = [[1,1,2], [3,4,5], [7,8,1]]
+
+print(True in (map(lambda x : 0 in x, a)))
+
+# if sum([False, False]) : print('dsa')
